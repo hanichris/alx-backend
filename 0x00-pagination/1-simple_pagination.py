160ddd4921dd6be6f08f419d56536c91c7b449d5
hanichris <hanichris71@gmail.com>
2023-02-20 23:46:31 +0300

    call the dataset method within get_page.

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -43,4 +43,5 @@ class Server:
         assert page > 0
         assert page_size > 0
         start_index, end_index = index_range(page, page_size)
-        return self.__dataset[start_index: end_index]
+
+        return self.dataset()[start_index: end_index]
