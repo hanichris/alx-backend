03037ac92c4a4a569bc022f2bafec56742dcf8c7
hanichris <hanichris71@gmail.com>
2023-02-21 03:41:02 +0300

    deletion-resilient pagination.

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import Dict, List, Union
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset: Union[List[List], None] = None
+        self.__indexed_dataset: Union[List[List], None] = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Deletion-resilient hypermedia pagination implementation.
+
+        Args:
+            index (int): start index of the returned page.
+            page_size (int): number of elements to list on the page.
+        Return:
+            dict: contains additional information about the returned page.
+        """
+        _data = self.indexed_dataset()
+        assert _data.get(index) is not None
+
+        data = []
+        j = index
+        while len(data) < page_size:
+            if j in _data:
+                data.append(_data.get(j))
+            j += 1
+
+        return {
+            'index': index,
+            'next_index': j,
+            'page_size': page_size,
+            'data': data
+        }
