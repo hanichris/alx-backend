63164b6fa7a81b55fe3cbc31f27ad6e3c21d1eff
hanichris <hanichris71@gmail.com>
2023-02-22 21:07:40 +0300

    FIFO cache implementation.

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Implementation of the FIFO cache.
+
+This cache behaves like the FIFO queue. Blocks are
+evicted in the order that they were added without
+regard to anything.
+"""
+from typing import Optional, Union
+
+
+BasicCache = __import__('0-basic_cache').BasicCache
+
+
+class FIFOCache(BasicCache):
+    """Implements the FIFO cache."""
+
+    def put(self, key: Optional[str], item: Optional[str]) -> None:
+        """Add an item to the cache as in a FIFO queue.
+
+        If the number of elements in the cache exceeds the set
+        `MAX_ITEMS`, first discard the first element in the cache.
+        Args:
+            key: index of the item to be added.
+            item: the value to be added at the given index.
+        """
+        if len(self.cache_data) > self.MAX_ITEMS:
+            first = next(iter(self.cache_data))
+            print(f"DISCARD: {self.cache_data.pop(first, None)}")
+
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key: Optional[str]) -> Union[str, None]:
+        """Return the value stored at the given key index.
+
+        Args:
+            key: index of interest.
+        Return:
+            str: value stored at the given key.
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key)
