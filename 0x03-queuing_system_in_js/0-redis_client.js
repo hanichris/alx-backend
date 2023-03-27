5b8f9362a81ed2a86a86c7cf25991abef19366c5
hanichris <hanichris71@gmail.com>
2023-03-27 19:23:10 +0300

    Add a 'connect' event handler on the client object.

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -6,6 +6,6 @@ client.on('error', err => {
   console.log(`Redis client not connected to the server: ${err}`);
 });
 
-await client.connect();
-
-console.log('Redis client connected to the server');
+client.on('connect', () => {
+  console.log('Redis client connected to the server')
+});
