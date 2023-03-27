78fef07ef549fd520482172f0b062626af5ba1cd
hanichris <hanichris71@gmail.com>
2023-03-27 19:24:39 +0300

    Fix eslint errors.

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -2,10 +2,10 @@ import { createClient } from 'redis';
 
 const client = createClient();
 
-client.on('error', err => {
+client.on('error', (err) => {
   console.log(`Redis client not connected to the server: ${err}`);
 });
 
 client.on('connect', () => {
-  console.log('Redis client connected to the server')
+  console.log('Redis client connected to the server');
 });
