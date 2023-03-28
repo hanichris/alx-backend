70d2ff851aac2fb58056f8d684b7b88e2945254e
hanichris <hanichris71@gmail.com>
2023-03-28 15:38:21 +0300

    Fix eslint error `comma-spacing` at line 17:40

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -14,7 +14,7 @@ client.hset('HolbertonSchools', 'Portland', '50', print);
 client.hset('HolbertonSchools', 'Seattle', '80', print);
 client.hset('HolbertonSchools', 'New York', '20', print);
 client.hset('HolbertonSchools', 'Bogota', '20', print);
-client.hset('HolbertonSchools', 'Cali' , '40', print);
+client.hset('HolbertonSchools', 'Cali', '40', print);
 client.hset('HolbertonSchools', 'Paris', '2', print);
 
 client.hgetall('HolbertonSchools', (err, reply) => {
