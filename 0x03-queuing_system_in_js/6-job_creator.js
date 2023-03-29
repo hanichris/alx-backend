e228749c874c6f6dc9b9360fb5e66970de80d013
hanichris <hanichris71@gmail.com>
2023-03-29 22:42:47 +0300

    fix eslint error 9:66 `prefer arrow callback.`

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -6,7 +6,7 @@ const jobData = {
   message: 'This is the code to verify your account',
 };
 
-const job = queue.create('push_notification_code', jobData).save(function(err) {
+const job = queue.create('push_notification_code', jobData).save((err) => {
   if (!err) {
     console.log(`Notification job created: ${job.id}`);
   }
